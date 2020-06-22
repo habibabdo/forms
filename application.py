c15edca558d350f9633b16ae9f35cf99c44dee81
habibabdo <habibdesigner9971728@gmail.com>
2020-06-22 10:46:49 +0300

    application.py  changed

--- application.py
+++ application.py
@@ -1,6 +1,4 @@
-
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,7 +8,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/more')
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form.get('name')
+    return render_template('hello.html', name=name)
 
-def more():
-    return render_template('more.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
