a4a2aee4fd2a44c7dc17bf61c93a15ff335615c5
habibabdo <habibdesigner9971728@gmail.com>
2020-06-22 11:10:14 +0300

    application.py changed

--- application.py
+++ application.py
@@ -8,10 +8,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello', methods=['POST'])
+@app.route('/hello', methods=["GET", "POST"])
 def hello():
-    name = request.form.get('name')
-    return render_template('hello.html', name=name)
+    if request.methods=="GET":
+        return "PLZ Submit the form instead"
+    else:
+        name = request.form.get('name')
+        return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
